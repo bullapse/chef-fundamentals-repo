391d41441141e8fe6ae3e6b532742919e256810e
SpencerGBull <Spencer.G.Bull@Target.com>
2015-06-18 14:59:31 -0500

    fixed and working with kitchen verify 2

--- cookbooks/apache/test/integration/default/localhost/bears_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'serverspec'
-
-set :backend, :execute
-
-describe 'bears site' do
-  it 'responds on port 81' do
-    expect(port 81).to be_listening 'tcp'
-  end
-end
--- cookbooks/apache/test/integration/default/localhost/clowns_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'serverspec'
-
-set :backend, :execute
-
-describe 'clowns site' do
-  it 'responds on port 80' do
-    expect(port 80).to be_listening 'tcp'
-  end
-end
